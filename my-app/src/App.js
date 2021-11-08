83458963ba16b4ea92eebf6f2f0a1609e3fbee01
fakehungry <chaikitr@gmail.com>
2021-11-08 16:29:25 +0700

    add components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,14 @@
+import Todo from "./components/Todo";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <Todo text="Hello" />
+      <Todo text="Hi" />
+      <Todo text="Sawasdee" />
+    </div>
+  );
 }
 
 export default App;
---
+++ my-app/src/components/Modal.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Modal() {
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button className="btn">Confirm</button>
+      <button className="btn btn--alt">Cancel</button>
+    </div>
+  );
+}
+
+export default Modal;
