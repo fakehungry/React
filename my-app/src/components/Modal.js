69ef04dec5bd0e1146ef9ec468e43739c4126e92
fakehungry <chaikitr@gmail.com>
2021-11-09 15:26:10 +0700

    last commit as exam

--- my-app/src/components/Modal.js
+++ my-app/src/components/Modal.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-function Modal() {
+function Modal(props) {
+  function cancelHandler() {
+    props.onCancel();
+  }
+
+  function confirmHandler() {
+    props.onConfirm();
+  }
+
   return (
     <div className="modal">
       <p>Are you sure?</p>
-      <button className="btn">Confirm</button>
-      <button className="btn btn--alt">Cancel</button>
+      <button className="btn" onClick={confirmHandler}>
+        Confirm
+      </button>
+      <button className="btn btn--alt" onClick={cancelHandler}>
+        Cancel
+      </button>
     </div>
   );
 }
